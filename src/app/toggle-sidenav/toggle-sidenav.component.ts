10456ba40c970559f064e8f3b4e3ca1b12045a62
Anton Augustsson <anton.augustsson99@gmail.com>
2022-12-10 17:48:38 +0100

    toggle sidenav changes direction and github actions for build

--- src/app/toggle-sidenav/toggle-sidenav.component.ts
+++ src/app/toggle-sidenav/toggle-sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-sidenav',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./toggle-sidenav.component.css']
 })
 export class ToggleSidenavComponent {
+  @Input() direction: boolean = false;
+
 
 }
